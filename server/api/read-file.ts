f464b76dfa329eef50ba83e4648f89567a87c74e
Bastian Friedrich <bastian.friedrich@cheers.io>
2022-09-08 16:21:03 +0200

    rm nitro config and move server files to server/assets

--- server/api/read-file.ts
+++ server/api/read-file.ts
@@ -1,7 +1,9 @@
 export default defineEventHandler(async (event) => {
-  const data = await useStorage().getItem('fs:hello.txt');
+  const data = await useStorage().getItem('assets:server:hello.txt');
+  const subData = await useStorage().getItem('assets:server:sub/hello.txt');
 
   return {
     data,
+    subData,
   };
 });
